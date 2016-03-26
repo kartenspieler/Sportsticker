8449eb4c8b12bd6eafe43fd07b9adfee332c6a48
Björn von der Linde <bjoern-hendrik.von.der.linde@exhumedo.de>
2016-03-26 16:23:08 +0100

    first steps to user registration

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var validator = require('../validators.js');
 
-/* GET users listing. */
 router.get('/create', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('userCreate');
+});
+
+router.post('/create', function(req, res, next) {
+	var email = req.body.email;
+	var password = req.body.password;
+	if (validator.isValidEmail(email) && password.length > 6) {
+		res.send('good');
+	} else {
+		
+	}
 });
 
 module.exports = router;
